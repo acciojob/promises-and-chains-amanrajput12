3c512fdca35878c9be27a759d93b87af378cc550
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-26 13:15:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+let age = document.querySelector("#age");
+let name = document.querySelector("#name");
+
+let btnclick = document.querySelector("#btn");
+
+function checkAge(age){
+	return new Promise((resolve,reject)=>{
+		 setTimeout(()=>{
+			 if(age>=18){
+            resolve("Welcome,. You can vote.");
+			 }
+			 else{
+				 reject("oh sorry. You aren't old enough.")
+			 }
+			 
+},4000)
+	})
+}
+
+btnclick.addEventListener("click",async(e)=>{
+	e.preventDefault()
+// alert(age.value)
+	if(!(age.value) || !(name.value)){
+		alert("Please enter valid details.")
+		return
+	}
+	else{
+		try{
+		let message = await checkAge(age.value);
+			alert(message);
+		}
+		catch(error){
+			alert(error)
+		}
+		
+	}
+	
+})
